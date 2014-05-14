d5d89c4240cbb9b2cdb58112583059c16d5de4f7
mfdiaspinto <mfdiaspinto@gmail.com>
2014-05-14 21:03:16 +0100

    Init Project Node Server
    Init Project Node Server

---
+++ PriNodeServer/PriNodeServer/server.js
@@ -0,0 +1,26 @@
+﻿var http = require('http');
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (req, res) {
+    res.send({ name: "Primavera Services" })
+});
+
+app.get("/netsales", function (req, res) {
+    res.send({ value: 120912 })
+});
+
+app.get("/netsales/:company", function (req, res) {
+    var company = req.params.company;
+    if (company == "Demo") {
+        res.send({ value: 10000 });
+    }
+    else {
+        res.send({ value: 99999 });
+    }
+});
+
+var server = http.createServer(app);
+
+server.listen(36500);
